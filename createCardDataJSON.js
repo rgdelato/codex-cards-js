4b7f0269f298d3b018f4e28bdad09cc451b0424e
Ryan De La Torre <rgdelato@Ryans-MacBook-Pro.local>
2016-05-24 17:12:24 -0700

    moving card data processing into a build script

---
+++ createCardDataJSON.js
@@ -0,0 +1,77 @@
+var fs = require('fs');
+
+var heroesJSON  = require('./src/json/heroes.json');
+var neutralJSON = require('./src/json/neutral.json');
+var redJSON     = require('./src/json/red.json');
+var greenJSON   = require('./src/json/green.json');
+var blueJSON    = require('./src/json/blue.json');
+var blackJSON   = require('./src/json/black.json');
+var whiteJSON   = require('./src/json/white.json');
+var purpleJSON  = require('./src/json/purple.json');
+var rulingsJSON = require('./src/json/rulings.json');
+
+
+
+const rulings = Object.keys(rulingsJSON).reduce((acc, key) => {
+	const rulingsByCard = rulingsJSON[key].reduce((acc, ruling) => {
+		if (!acc[ruling.card]) { acc[ruling.card] = []; }
+		acc[ruling.card].push(ruling);
+		return acc;
+	}, {});
+
+	return {...acc, ...rulingsByCard};
+}, {});
+
+
+const cardsJSON = [].concat(heroesJSON, neutralJSON, redJSON, greenJSON, blueJSON, blackJSON, whiteJSON, purpleJSON);
+
+let data = cardsJSON.reduce((acc, item) => {
+	const { cards, specs, heroes, colors, starters, urlCardToCard, urlColorToColor, urlColorToSpecs } = acc;
+
+	//
+	item.rulings = rulings[item.name];
+
+	//
+	cards[item.name] = item;
+
+	//
+	if (item.spec) {
+		if (!specs[item.spec]) { specs[item.spec] = []; }
+		specs[item.spec].push(item.name);
+	}
+
+	//
+	if (item.type === 'Hero' && item.spec) {
+		heroes[item.spec] = item.name;
+	}
+
+	//
+	if (item.color) {
+		if (!colors[item.color]) { colors[item.color] = []; }
+		colors[item.color].push(item.name);
+	}
+
+	//
+	if (item.starting_zone === 'deck' && item.color) {
+		if (!starters[item.color]) { starters[item.color] = []; }
+		starters[item.color].push(item.name);
+	}
+
+	//
+	const urlName = item.name.toLowerCase().replace(/\s/g, '_');
+	urlCardToCard[urlName] = item.name;
+
+	if (item.color && item.spec) {
+		const urlColor = item.color.toLowerCase();
+		urlColorToColor[urlColor] = item.color;
+
+		if (!urlColorToSpecs[urlColor]) { urlColorToSpecs[urlColor] = []; }
+		if (!urlColorToSpecs[urlColor].includes(item.spec)) { urlColorToSpecs[urlColor].push(item.spec); }
+	}
+
+	return acc;
+}, { cards: {}, specs: {}, heroes: {}, colors: {}, starters: {}, urlCardToCard: {}, urlColorToColor: {}, urlColorToSpecs: {} });
+
+
+
+fs.writeFileSync('src/cardData.json', JSON.stringify(data, null, '  '));
