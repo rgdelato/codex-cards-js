68d7a448a55715335991ef27115712fab153508f
rgdelato <rgdelato@gmail.com>
2016-05-24 22:16:07 -0700

    deck page finally looking half-way decent across devices

--- src/components/DeckPage.js
+++ src/components/DeckPage.js
@@ -23,16 +23,16 @@ var DeckPage = ({ route, params }) => {
 
 			<div className="starter">
 				<div className="worker-image" style={{ display: 'inline-block', verticalAlign: 'top' }}>
-					<div style={{display: 'table'}}>
-						<div style={{display: 'table-cell', width: '330px', height: '450px', verticalAlign: 'middle'}}>
+					<div className="table-hack">
+						<div className="table-cell-hack" style={{ width: '330px', height: '450px' }}>
 							<h2>{starter} Starter</h2>
 						</div>
 					</div>
 				</div>
 
-				<div className="starter-deck-list" style={{ display: 'inline-block', verticalAlign: 'top' }}>
-					<div style={{display: 'table'}}>
-						<div style={{display: 'table-cell', width: '330px', height: '450px', verticalAlign: 'middle', textAlign: 'left'}}>
+				<div className="starter-list" style={{ display: 'inline-block', verticalAlign: 'top' }}>
+					<div className="table-hack">
+						<div className="table-cell-hack" style={{ width: '330px', height: '450px' }}>
 							<blockquote>
 								{starters[starter].map((name) => {
 									var card = cards[name];
@@ -53,22 +53,22 @@ var DeckPage = ({ route, params }) => {
 					var hero = cards[heroes[spec]];
 					var cardNames = specs[spec];
 					return (
-						<div className="spec" key={spec} style={{ display: 'inline-block', width: '33%', verticalAlign: 'top' }}>
+						<div className="spec" key={spec} style={{ display: 'inline-block', verticalAlign: 'top' }}>
 							<div className="card-image-container">
 								<Link to={"/card/" + toURL(hero.name)}><img className="card-image" src={"http://codexcards-assets.surge.sh/images/" + hero.sirlins_filename} /></Link>
 							</div>
 
-							<div className="spec-deck-list" style={{padding: '0 16px', textAlign: 'left'}}>
-							<blockquote>
-								{cardNames.map((name) => {
-									var card = cards[name];
-									return (
-										<div key={name} style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>
-											<Link to={"/card/" + toURL(card.name)}>{card.name}</Link> - <small>{card.type}</small>
-										</div>
-									);
-								})}
-							</blockquote>
+							<div className="spec-list">
+								<blockquote>
+									{cardNames.map((name) => {
+										var card = cards[name];
+										return (
+											<div key={name} style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}}>
+												<Link to={"/card/" + toURL(card.name)}>{card.name}</Link> - <small>{card.type}</small>
+											</div>
+										);
+									})}
+								</blockquote>
 							</div>
 						</div>
 					);
